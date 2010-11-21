446f6aff3a38db06db570eba47469db1716cf2eb
Dave Rolsky <autarch@urth.org>
2010-11-21 12:38:01 -0600

    Add docs

--- lib/Antispam/httpBL.pm
+++ lib/Antispam/httpBL.pm
@@ -64,3 +64,69 @@ __PACKAGE__->meta()->make_immutable();
 1;
 
 # ABSTRACT: Antispam checks using Project Honeypot's http blacklist
+
+__END__
+
+=head1 SYNOPSIS
+
+  my $bl = Antispam::httpBL->new( access_key => 'abc123' );
+
+  my $result = $bl->check_user( ip => '1.2.3.4' );
+
+  if ( $result->score() ) { ... }
+
+=head1 DESCRIPTION
+
+This module implements the L<Antispam::Toolkit::Role::UserChecker> role using
+Project Honeypot's Http:BL API to check whether a given IP address is
+associated with spamming or email harvesting.
+
+=head1 METHODS
+
+This class provides the following methods:
+
+=head2 Antispam::httpBL->new( access_key => ... )
+
+This method constructs a new object. It requires an access key. You can get an
+access key from the Project Honeypot website at
+L<http://www.projecthoneypot.org/>.
+
+=head2 $bl->check_user( ip => ... )
+
+This method checks whether an ip address is associated with some sort of
+spam-related behavior.
+
+It returns an L<Antispam::Toolkit::Result> object.
+
+While the Http:BL API allows for threat scores from 0-255, the result will
+contain a score from 0-10. This score is the Http:NL threat score divided by
+7.5, and capped at 10.
+
+The details in the result object will break down all the results returned by
+the Http:BL API.
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-antispam-httpbl@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
+notified of progress on your bug as I make changes.
+
+=head1 DONATIONS
+
+If you'd like to thank me for the work I've done on this module, please
+consider making a "donation" to me via PayPal. I spend a lot of free time
+creating free software, and would appreciate any support you'd care to offer.
+
+Please note that B<I am not suggesting that you must do this> in order for me
+to continue working on this particular software. I will continue to do so,
+inasmuch as I have in the past, for as long as it interests me.
+
+Similarly, a donation made in this way will probably not make me work on this
+software much more, unless I get so many donations that I can consider working
+on free software full time, which seems unlikely at best.
+
+To donate, log into PayPal and send money to autarch@urth.org or use the
+button on this page: L<http://www.urth.org/~autarch/fs-donation.html>
+
+=cut
